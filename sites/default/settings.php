3e0bd35cd5a920c3183deae121ca1ca6549fdb60
thegreeneyl <service@thegreeneyl.com>
2019-08-01 15:13:07 +0200

    not relevant

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -729,6 +729,10 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  * example.org, with all subdomains included.
  */
 
+$settings['trusted_host_patterns'] = array(
+  '^api\.thedynamicarchive\.net$',
+);
+
 /**
  * The default list of directories that will be ignored by Drupal's file API.
  *
@@ -778,9 +782,9 @@ $settings['entity_update_backup'] = TRUE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 $databases['default']['default'] = array (
-  'database' => 'dynamicarchive-v1-9',
-  'username' => 'root',
-  'password' => 'root',
+  'database' => 'web95_db1',
+  'username' => 'web95',
+  'password' => 'Vv4Fdefi',
   'prefix' => '',
   'host' => 'localhost',
   'port' => '3306',
