80942fa5c81466d9cf231742858d7559f1f4096e
Willy <willy@thegreeneyl.com>
2019-08-14 11:02:08 +0200

    fix array bug

--- modules/workflow/src/Plugin/rest/resource/ComponentOrderResource.php
+++ modules/workflow/src/Plugin/rest/resource/ComponentOrderResource.php
@@ -66,7 +66,7 @@ class ComponentOrderResource extends ResourceBase {
       $query->sort('tid', 'ASC');
 
 	  $tids = $query->execute();
-	  if (is_array($tids)) return NULL;
+	  if (!is_array($tids)) return NULL;
 
 	  $terms = Term::loadMultiple($tids);
 	  return $terms;
