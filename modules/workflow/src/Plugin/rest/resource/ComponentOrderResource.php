86eb41741e4738bb435bfeebeec318df61c1e8e4
Willy <willy@thegreeneyl.com>
2019-08-14 11:00:43 +0200

    check for correct post data

--- modules/workflow/src/Plugin/rest/resource/ComponentOrderResource.php
+++ modules/workflow/src/Plugin/rest/resource/ComponentOrderResource.php
@@ -83,7 +83,7 @@ class ComponentOrderResource extends ResourceBase {
     }
     $response = $data;
     
-  	if (array_key_exists("id", $data)) {
+  	if (is_array($data) && array_key_exists("id", $data)) {
 	  	$group_id = intval($data["id"]);
 
 	    if (is_numeric($group_id)) $group = Group::load($group_id);
@@ -101,7 +101,7 @@ class ComponentOrderResource extends ResourceBase {
 
 			$tweight = 0;
 
-			foreach ($data["groups"] as $key => $targetterm) {
+			if (is_array($terms)) foreach ($data["groups"] as $key => $targetterm) {
 			  	if (array_key_exists($targetterm['tid'], $terms)) {
 			  		$term = $terms[$targetterm['tid']];
 
