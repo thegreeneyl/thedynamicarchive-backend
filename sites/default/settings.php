99addc98e9e8555fa3696a721cece227225337d3
thegreeneyl <service@thegreeneyl.com>
2019-10-18 00:06:20 +0200

    update

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -731,6 +731,7 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 
 $settings['trusted_host_patterns'] = array(
   '^api\.thedynamicarchive\.net$',
+  '^api\.thedynamicarchive\.org$',
 );
 
 /**
